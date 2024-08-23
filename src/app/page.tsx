8380a14cecb5c6b62b6dc7d5c5ab6f238b8d5d89
Gustavo Garcia Gabriel <gus16710@gmail.com>
2024-08-23 13:04:18 -0600

    adding use client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-export default function Home() {
+"use client";
+
+export default function Home() { 
   return (
     <main className="">
       <iframe
+        id="biz-xalapa"
         allowFullScreen={true}
         scrolling="no"
         className="h-screen w-full"
         src="https://heyzine.com/flip-book/6c0512df26.html"
-        //style="border: 0px; width: 100%; height: 600px;"
       ></iframe>
     </main>
   );
