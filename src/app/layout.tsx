b6e7fbdb9ac3bb0e7967d883f3b9d7309b6795a7
Gustavo <gus16710@gmail.com>
2025-01-08 18:55:54 -0600

    Whatsapp channel unaideamas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: "✨ Una Idea Más 🎨",
     description: "¡Bienvenido al canal donde la inspiración nunca termina! 🚀 Aquí encontrarás una colección exclusiva de diseños gráficos, vectores editables y recursos creativos ideales para diseñadores, ilustradores y amantes del arte visual.",
-    images: ["https://bizxalapa.unaideamas.com/img/image.jpg"],
+    images: ["https://bizxalapa.unaideamas.com/img/front-image.jpg"],
   },
   metadataBase: new URL("https://bizxalapa.unaideamas.com"),
 };
